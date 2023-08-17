dbe0d6756bcf2e5f4879f662c194866fcae5ebe1
AndreyAlth <andreyalth@gmail.com>
2023-08-16 20:57:23 -0600

    agregar titulo de pagina

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,15 @@
 import '@/styles/globals.css'
 import Layout from '@/components/layout/Layout'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
-  return <Layout>{getLayout(<Component {...pageProps} />)}</Layout>
+  return (
+    <>
+      <Head>
+        <title>andreyalth.site</title>
+      </Head>
+      <Layout>{getLayout(<Component {...pageProps} />)}</Layout>
+    </>
+  )
 }
